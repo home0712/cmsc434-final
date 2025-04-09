2246eaf682c1bc09ab3cbfd244b6b1b5e0637065
home0712 <k07m12j@gmail.com>
2025-04-09 02:21:57 -0400

    added keyboard

--- tabs/tab2-record/tab2-common.js
+++ tabs/tab2-record/tab2-common.js
@@ -135,3 +135,33 @@ function getIconForCategory(category) {
     
     return iconMap[category] || "Clipboard.png";
 }
+
+// keyboard
+function showKeyboard() {
+    const keyboard = document.getElementById("keyboard");
+    const content = document.querySelector(".device-content");
+    keyboard.style.display = "block";
+    content.style.paddingBottom = "320px"; 
+    keyboard.style.height = "350px";
+}
+  
+function hideKeyboard() {
+    const keyboard = document.getElementById("keyboard");
+    const content = document.querySelector(".device-content");
+    keyboard.style.display = "none";
+    content.style.paddingBottom = "75px";
+    keyboard.style.height = "0px";
+}
+
+document.querySelectorAll("input, textarea").forEach(field => {
+    field.addEventListener("focus", () => {
+      showKeyboard();
+      setTimeout(() => {
+        field.scrollIntoView({ behavior: "smooth", block: "center" });
+      }, 200);
+    });
+  
+    field.addEventListener("blur", () => {
+      hideKeyboard();
+    });
+});
