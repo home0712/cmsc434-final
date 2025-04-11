fdf07aa13822dab28f9beb73f0071ef0771b442c
home0712 <k07m12j@gmail.com>
2025-04-10 21:20:36 -0400

    added js function & css to home

--- tabs/tab1-home/home.js
+++ tabs/tab1-home/home.js
@@ -1,44 +1,31 @@
 /* MAIN - HOME TAB */
 document.addEventListener("DOMContentLoaded", () => {
     loadDefaultData();
+    bindButtons();
+    renderCurrentMonth();
+    renderSummary();
 });
 
-
-/* 
-    click the icons to navigate to dedicated screens 
-    (filter, add, search)
-*/
-const leftButtons = document.querySelectorAll(".title-button");
-
-for (const button of leftButtons) {
-    button.addEventListener("click", () => {
-        const id = button.id;
-        if (id === "summary-button") {
-            window.location.href = "../tab2-record/transaction/transaction.html";
-        } else if (id === "account-button") {
-            window.location.href = "../tab2-record/accounts/accounts.html";
-        } else if (id === "spending-button") {
-            window.location.href = "../tab4-report/category/category.html";
-        }
-    });
+function bindButtons() {
+    const leftButtons = document.querySelectorAll(".title-button");
+    leftButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const id = button.id;
+            if (id === "summary-button") {
+                window.location.href = "../tab2-record/transaction/transaction.html";
+            } else if (id === "account-button") {
+                window.location.href = "../tab2-record/accounts/accounts.html";
+            } else if (id === "spending-button") {
+                window.location.href = "../tab4-report/category/category.html";
+            }
+        });
+    })
 }
 
-/*
-    show the current month
- */
+// show the current month
 const monthNames = [
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June",
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
 ];
 
 function renderCurrentMonth() {
@@ -50,5 +37,42 @@ function renderCurrentMonth() {
     titleText.textContent = `${currentMonth} ${currentYear} Summary`;
 }
 
-renderCurrentMonth();
+// 
+function renderSummary() {
+    const currentMonth = new Date().getMonth() + 1;
+    const localLogs = JSON.parse(localStorage.getItem("transactions")) || [];
+
+    let totalIncome = 0.00;
+    let totalExpense = 0.00;
+
+    localLogs.forEach(log => {
+        if (currentMonth === Number(log.date.slice(5, 7))) {
+            if (log.type === "INCOME") {
+                totalIncome += Math.abs(log.amount);
+            } else {
+                totalExpense += Math.abs(log.amount);
+            }
+        }
+    });
+
+    const totalNet = totalIncome - totalExpense;
+    document.getElementById("income-value").textContent = totalIncome.toLocaleString();
+    document.getElementById("expense-value").textContent = totalExpense.toLocaleString();
+    document.getElementById("net-container").innerHTML = 
+        totalNet > 0 
+        ?   `<span class="dollar-sign">+ $</span>
+            <span class="amount-value">${Math.abs(totalNet)}</span>
+            ` 
+        : totalNet === 0 
+        ?   `<span class="dollar-sign"> $</span>
+            <span class="amount-value">0</span>
+            ` 
+        :
+            `<span class="dollar-sign">- $</span>
+            <span class="amount-value">${Math.abs(totalNet)}</span>
+            ` 
+
+                
+}
+
 
